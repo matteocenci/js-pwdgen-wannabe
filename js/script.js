b492b87024ddca8a1b29fb752791731f0eeeef49
matteocenci <matteocenci01@gmail.com>
2024-03-22 15:25:12 +0100

    feat: print document in page

--- js/script.js
+++ js/script.js
@@ -6,11 +6,17 @@
 let userName = prompt("Ciao! Come ti chiami?");
 console.log(userName);
 
+// Chiedi all'utente il cognome
 let userSurname = prompt("E invece il tuo cognome?");
 console.log(userSurname);
 
+// Chiedi il colore preferito
 let favColor = prompt("Qual'è il tuo colore preferito?");
 console.log(favColor);
 
+// Test di combinazione
 const finalTest = userName + userSurname + favColor + '21';
 console.log(finalTest);
+
+// Scrivi documento in pagina
+document.getElementById("final").innerHTML = finalTest;
