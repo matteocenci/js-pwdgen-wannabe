9515c6746036b29d05e7aaa763bf6b976560f7ea
matteocenci <matteocenci01@gmail.com>
2024-03-22 15:13:12 +0100

    feat: ask surname

--- js/script.js
+++ js/script.js
@@ -4,4 +4,7 @@
 
 // Chiedi all'utente il nome
 let userName = prompt("Ciao! Come ti chiami?")
-// console.log(userName, typeof userName);
+console.log(userName)
+
+let userSurname = prompt("E invece il tuo cognome?")
+console.log(userSurname)
