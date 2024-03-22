192c0e44111ce6af2299bfc4c1f0a104badeb912
matteocenci <matteocenci01@gmail.com>
2024-03-22 15:17:04 +0100

    feat: Color and final test

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@
 
 
 // Chiedi all'utente il nome
-let userName = prompt("Ciao! Come ti chiami?")
-console.log(userName)
+let userName = prompt("Ciao! Come ti chiami?");
+console.log(userName);
 
-let userSurname = prompt("E invece il tuo cognome?")
-console.log(userSurname)
+let userSurname = prompt("E invece il tuo cognome?");
+console.log(userSurname);
+
+let favColor = prompt("Qual'è il tuo colore preferito?");
+console.log(favColor);
+
+const finalTest = userName + userSurname + favColor + '21';
+console.log(finalTest);
